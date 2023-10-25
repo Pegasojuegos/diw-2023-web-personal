c0fbb6c8aabedf598cc674f740db1e92882c0d2d
pegaso <mhp74185@gmail.com>
2023-10-25 14:18:42 +0200

    casasSeAbren

--- pegasojuegos/js/javascript.js
+++ pegasojuegos/js/javascript.js
@@ -7,3 +7,41 @@ function cambiarTema() {
     body[0].classList.replace('tema-oscuro','tema-claro')
   }
 };
+
+//Asigno un evento de click para abrir las cajas
+let cajas=document.querySelectorAll(".cajita.afic");
+cajas.forEach(elemento => {
+  elemento.addEventListener("click",abrirCaja);
+})
+
+let contenidos=new Map;
+contenidos.set("idjuegos","<div>• Celeste<br/>• Live is strange<br/>• Battlefront II<br/>• League of Legends</div>");
+contenidos.set("idescalada","<div>• Boulder<br/>• Senderismo<br/>• Vivac en el bosque<br/>• Bushcraft</div>");
+contenidos.set("idleer","Leer");
+contenidos.set("idcocinar","Cocinar");
+
+
+
+function abrirCaja(){
+  this.classList.add("botonSelec");
+  cajas.forEach(elemento=>{
+    if(elemento!==this)elemento.classList.add("botonNoSelec");
+    else {
+      elemento.addEventListener("click",cerrarCaja)
+      elemento.innerHTML=contenidos.get(elemento.id);
+    };
+  })
+}
+
+function cerrarCaja(){
+  this.classList.remove("botonSelec");
+  cajas.forEach(elemento=>{
+    if(elemento!==this)elemento.classList.remove("botonNoSelec");
+    else{
+      elemento.innerHTML="" 
+      elemento.removeEventListener("click",cerrarCaja);
+      elemento.addEventListener("click",abrirCaja);
+
+    }
+  })
+}
