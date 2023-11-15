5bf1949cd4ac942b1630d011a9585633c0d6ebf1
pegasojuegos <mhp74185@gmail.com>
2023-11-15 20:37:53 +0100

    Web finalizada

--- pegasojuegos/js/javascript.js
+++ pegasojuegos/js/javascript.js
@@ -1,5 +1,6 @@
 const body = document.getElementsByTagName('body');
 
+//Función para cambiar de tema claro a tema oscuro
 function cambiarTema() {
   if(body[0].classList.contains('tema-claro')){
     body[0].classList.replace('tema-claro','tema-oscuro')
@@ -14,14 +15,18 @@ cajas.forEach(elemento => {
   elemento.addEventListener("click",abrirCaja);
 })
 
+//Mapa que contendrá el contenido de cada caja
 let contenidos=new Map;
 contenidos.set("idjuegos","<div>• Celeste<br/>• Live is strange<br/>• Battlefront II<br/>• League of Legends</div>");
 contenidos.set("idescalada","<div>• Boulder<br/>• Senderismo<br/>• Vivac en el bosque<br/>• Bushcraft</div>");
 contenidos.set("idleer","<div>• To your eternity<br/>• Solanin<br/>• El Hobitt<br/>• El señor de los anillo<br/>• Buenas noches PunPun</div>");
 contenidos.set("idcocinar","<div>Me gusta cocinar platos complejos y experimentar con sabores de diferentes culturas. Siempre intento inovar y darle un toque diferente a cada plato.</div>");
 
-
-
+/*
+Función que añade una clase a la caja que ha provocado el evento la cual lo expandira, y 
+añadirá una clase al resto de cajas para que desaparezcan.
+Además añadirá un nuevo evento a la caja seleccionada el cual llama a cerrarCaja()
+*/
 function abrirCaja(){
   this.classList.add("botonSelec");
   cajas.forEach(elemento=>{
@@ -33,6 +38,8 @@ function abrirCaja(){
   })
 }
 
+/*Función que deshace todo lo hecho por la anterior clase, devolviendo todasl las
+cajas a la normalidad*/
 function cerrarCaja(){
   this.classList.remove("botonSelec");
   cajas.forEach(elemento=>{
